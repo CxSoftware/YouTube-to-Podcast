c0026b3214587486dfa88a0b19e0ef7d3c98911c
Juan C. Olivares <cristobal@cxsoftware.com>
2017-07-25 23:59:56 -0400

    Adding log configuration for local (file), console and cloudwatch

--- src/util/log.js
+++ src/util/log.js
@@ -1,9 +1,24 @@
 // Dependencies
 const path = require ('path');
 const winston = require ('winston');
+const winstoncw = require ('winston-cloudwatch');
 
-// Get paths
-const LOG_PATH = path.join (__dirname, '..', '..', 'service.log');
+// Local
+const config = require ('./config');
 
-// Configure log
-winston.add (winston.transports.File, { filename: LOG_PATH });
+// Path
+config.log.local.filename = path.join (__dirname, '..', '..', config.log.local.filename);
+
+// Configure
+// Console
+winston.remove (winston.transports.Console);
+if (config.log.console.enabled)
+	winston.add (winston.transports.Console, config.log.console);
+
+// Local
+if (config.log.local.enabled)
+	winston.add (winston.transports.File, config.log.local);
+
+// Cloud watch
+if (config.log.cloudwatch.enabled)
+	winston.add (winstoncw, config.log.cloudwatch);
