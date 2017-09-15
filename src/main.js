5a803f37f7dec0c6d45684549cc8b93f267a98a0
Juan C. Olivares <cristobal@cxsoftware.com>
2017-09-15 02:29:32 -0300

    Adding configuration Adding main workflow

--- src/main.js
+++ src/main.js
@@ -6,14 +6,67 @@ require ('./util/log');
 const winston = require ('winston');
 
 // Local
-const config = require ('./util/config');
+import {Aws} from './util/aws';
+import {Podcast} from './util/podcast';
+import {YouTube} from './util/youtube';
 
 (async () =>
 {
 	try
 	{
-		winston.log ('info', 'Hello world!');
-		winston.log ('info', 'Config', config);
+		// Init
+		winston.info ('Initializing');
+		const youtube = new YouTube ();
+		const podcast = new Podcast ();
+		const aws = new Aws ();
+		let uploaded = 0;
+
+		// Get data from YouTube
+		winston.info ('Getting data from channel');
+		const items = await youtube.getVideoList ();
+
+		// Add items
+		winston.info ('Adding items');
+		for (const item of items)
+		{
+			// Create filename
+			const filename = `${item.id}.aac`;
+
+			// Upload if it doesn't exists in S3
+			if (!(await aws.fileExists (filename)))
+			{
+				winston.info ('Downloading', item.title, filename);
+				const stream = await youtube.downloadVideo (item.id);
+				winston.info ('Uploading');
+				await aws.upload (stream, filename);
+				uploaded++;
+			}
+
+			// Get info
+			const length = await aws.getLength (filename);
+
+			// Add to feed
+			winston.info ('Adding to feed', item.title);
+			podcast.addItem (
+				item.title,
+				item.description,
+				aws.getUrl (filename),
+				item.date,
+				item.duration,
+				filename,
+				length);
+		}
+
+		// Generate?
+		if (!uploaded)
+		{
+			winston.info ('No files uploaded. Not updating feed.');
+			// return;
+		}
+
+		// Upload
+		winston.info ('Uploading feed');
+		aws.upload (podcast.generate (), 'feed.xml');
 	}
 	catch (e)
 	{
