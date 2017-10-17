5df557c5f4952befde8f3e59fa20121343af32bc
Juan C. Olivares <cristobal@cxsoftware.com>
2017-10-17 12:17:17 -0300

    Adding support to limit concurrent calls to YouTube API

--- src/util/youtube.js
+++ src/util/youtube.js
@@ -1,6 +1,7 @@
 // Dependencies
 const bluebird = require ('bluebird');
 const moment = require ('moment');
+const promiseLimit = require ('promise-limit');
 const winston = require ('winston');
 const youtubedl = require ('youtube-dl');
 const YouTubeNode = require ('youtube-node');
@@ -16,6 +17,7 @@ export class YouTube
 		this.config = config.youtube;
 		this.youTube = bluebird.promisifyAll (new YouTubeNode ());
 		this.youTube.setKey (this.config.key);
+		this.limit =  promiseLimit (this.config.limit);
 	}
 
 	async downloadVideo (id)
@@ -57,23 +59,26 @@ export class YouTube
 				description: item.snippet.description
 			}));
 
-		const items2 = await Promise.all (items.map (async (item) =>
-		{
-			winston.debug ('Getting details for', item.id);
-			const details = await this.youTube.getByIdAsync (item.id);
-			const durationString = details.items [0].contentDetails.duration;
-			const duration = moment
-				.duration (durationString)
-				.as ('seconds');
-			if (duration < this.config.minDuration)
-				return null;
-
-			item.duration = duration;
-			item.date = details.items [0].snippet.publishedAt;
-			item.filename = `${item.id}.aac`;
-			return item;
-		}));
+		const items2 = await Promise.all (items.map (item =>
+			this.limit (() => this.getDetails (item))));
 
 		return items2.filter (x => x);
+	}
+
+	async getDetails (item)
+	{
+		winston.debug ('Getting details for', item.id);
+		const details = await this.youTube.getByIdAsync (item.id);
+		const durationString = details.items [0].contentDetails.duration;
+		const duration = moment
+			.duration (durationString)
+			.as ('seconds');
+		if (duration < this.config.minDuration)
+			return null;
+
+		item.duration = duration;
+		item.date = details.items [0].snippet.publishedAt;
+		item.filename = `${item.id}.aac`;
+		return item;
 	}
 }
